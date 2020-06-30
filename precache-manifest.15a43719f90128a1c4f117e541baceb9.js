4e534729133154cb49a1e839c64231f2ad95f526
ElandVarse <andre.jobs23@gmail.com>
2020-06-30 14:56:22 -0300

    Updates

---
+++ precache-manifest.15a43719f90128a1c4f117e541baceb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03572a4401724252c8b9f44191862ac3",
+    "url": "/portalAluno/index.html"
+  },
+  {
+    "revision": "1f48c4e436c70ca57d9b",
+    "url": "/portalAluno/static/css/main.1a32ccaf.chunk.css"
+  },
+  {
+    "revision": "e8af92671aa21024c228",
+    "url": "/portalAluno/static/js/2.c2b494f1.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/portalAluno/static/js/2.c2b494f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f48c4e436c70ca57d9b",
+    "url": "/portalAluno/static/js/main.1ce2ad9c.chunk.js"
+  },
+  {
+    "revision": "baad8702acb4bb98f3e1",
+    "url": "/portalAluno/static/js/runtime-main.fc9174ec.js"
+  },
+  {
+    "revision": "f91b538cc146236cb2ba3aec0fc17149",
+    "url": "/portalAluno/static/media/undraw_fill.f91b538c.svg"
+  }
+]);
